1006546fe4cea9bdd6d2f0fb1db94f309858786e
aidangoldie7 <40402670@live.napier.ac.uk>
2021-03-10 15:34:59 +0000

    Commit 1.

---
+++ AidanTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AidanTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
